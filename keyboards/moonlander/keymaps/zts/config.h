f417df011d30f3a29c72deb70917d16eab850695
Zachary Stevens <zts@akoova.com>
2022-05-28 13:09:36 +0100

    Copy keymap from zts1 to zts
    The resulting firmware appears to work, but does not include per-key
    backlighting, and the default (full kb) animation is running on reboot.

--- keyboards/moonlander/keymaps/zts/config.h
+++ keyboards/moonlander/keymaps/zts/config.h
@@ -19,3 +19,15 @@
 #pragma once
 
 #define ORYX_CONFIGURATOR
+
+// START homerowmods
+// from https://precondition.github.io/home-row-mods
+#undef TAPPING_TERM
+#define TAPPING_TERM 216
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+// END homerowmods
--- keyboards/moonlander/keymaps/zts/keymap.c
+++ keyboards/moonlander/keymaps/zts/keymap.c
@@ -31,34 +31,78 @@ enum custom_keycodes {
     VRSN = ML_SAFE_RANGE,
 };
 
+#define KC_MAC_UNDO LGUI(KC_Z)
+#define KC_MAC_CUT LGUI(KC_X)
+#define KC_MAC_COPY LGUI(KC_C)
+#define KC_MAC_PASTE LGUI(KC_V)
+#define KC_PC_UNDO LCTL(KC_Z)
+#define KC_PC_CUT LCTL(KC_X)
+#define KC_PC_COPY LCTL(KC_C)
+#define KC_PC_PASTE LCTL(KC_V)
+
+// Left-hand home row mods
+#define CTL_A LCTL_T(KC_A)
+#define ALT_S LALT_T(KC_S)
+#define GUI_D LGUI_T(KC_D)
+#define SFT_F LSFT_T(KC_F)
+
+// Right-hand home row mods
+#define SFT_J RSFT_T(KC_J)
+#define GUI_K RGUI_T(KC_K)
+#define ALT_L LALT_T(KC_L)
+#define CTL_SCLN RCTL_T(KC_SCLN)
+
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [BASE] = LAYOUT_moonlander(
-        KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LEFT,           KC_RGHT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_DEL,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(SYMB),         TG(SYMB), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HYPR,           KC_MEH,  KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), LGUI_T(KC_QUOT),
-        KC_LSFT, LCTL_T(KC_Z),KC_X,KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), KC_RSFT,
-    LT(SYMB,KC_GRV),WEBUSB_PAIR,A(KC_LSFT),KC_LEFT, KC_RGHT,  LALT_T(KC_APP),    RCTL_T(KC_ESC),   KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(SYMB),
-                                            KC_SPC,  KC_BSPC, KC_LGUI,           KC_LALT,  KC_TAB,  KC_ENT
-    ),
-
-    [SYMB] = LAYOUT_moonlander(
-        VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,           _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,           _______, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
-        _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  _______,           _______, KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, _______,
-        _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                             KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, _______,
-        EEP_RST, _______, _______, _______, _______,          RGB_VAI,           RGB_TOG,          _______, KC_DOT,  KC_0,    KC_EQL,  _______,
-                                            RGB_HUD, RGB_VAD, RGB_HUI, TOGGLE_LAYER_COLOR,_______, _______
-    ),
-
-    [MDIA] = LAYOUT_moonlander(
-        LED_LEVEL,_______,_______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, RESET,
-        _______, _______, _______, KC_MS_U, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,           _______, _______, _______, _______, _______, _______, KC_MPLY,
-        _______, _______, _______, _______, _______, _______,                             _______, _______, KC_MPRV, KC_MNXT, _______, _______,
-        _______, _______, _______, KC_BTN1, KC_BTN2,         _______,            _______,          KC_VOLU, KC_VOLD, KC_MUTE, _______, _______,
-                                            _______, _______, _______,           _______, _______, _______
-    ),
+	[0] = LAYOUT_moonlander(
+    	KC_GRAVE,	KC_1,		KC_2,		KC_3,       KC_4,       KC_5,		KC_MINUS,			KC_EQUAL,	KC_6,		KC_7,       KC_8,		KC_9,		KC_0,    	KC_MINUS,       
+    	KC_TAB,		KC_Q,    	KC_W,       KC_E,       KC_R,       KC_T,       KC_LBRC,        	KC_RBRC,	KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,    	KC_BSLASH,      
+		ALL_T(KC_ESCAPE),CTL_A, ALT_S,      GUI_D,      SFT_F,      KC_G,       _______,            _______,	KC_H,       SFT_J,      GUI_K,      ALT_L,      CTL_SCLN,	KC_QUOTE,       
+		KC_LSHIFT,	KC_Z,    	KC_X,       KC_C,       KC_V,       KC_B,                                       KC_N,       KC_M,       KC_COMMA,   KC_DOT,     KC_SLASH,	KC_RSHIFT,      
+		KC_LCTRL,	KC_LALT, 	KC_LGUI,    _______,	MO(1),      _______,                                    _______,    MO(4),		_______,	KC_RGUI,    KC_RALT, 	KC_RCTRL,       
+                                            			KC_BSPACE,  MO(5),      MO(2),          	MO(3),   	KC_ENTER,   KC_SPACE
+	),
+	[1] = LAYOUT_moonlander(
+		TO(0),          _______, _______, _______, _______, _______, _______,                                 _______, _______, _______, _______, _______, _______, _______, 
+		_______, _______, _______, _______, _______, _______, _______,                                 _______, LCTL(KC_LEFT),  LGUI(KC_LBRACKET),LGUI(KC_RBRACKET),RCTL(KC_RIGHT), _______, _______, 
+		_______, KC_LCTRL,       KC_LALT,        KC_LGUI,        KC_LSHIFT,      _______, _______,                                                                 _______, KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       _______, _______, 
+		_______, KC_MAC_UNDO,    KC_MAC_CUT,     KC_MAC_COPY,    KC_MAC_PASTE,   _______,                                 KC_HOME,        KC_PGDOWN,      KC_PGUP,        KC_END,         _______, _______, 
+		_______, _______, _______, _______, _______, _______,                                                                                                 _______, _______, _______, _______, _______, _______, 
+		_______, _______, _______,                 _______, _______, _______
+	),
+	[2] = LAYOUT_moonlander(
+		TO(0),          _______, _______, _______, _______, _______, _______,                                 KC_AUDIO_MUTE,  _______, _______, _______, _______, WEBUSB_PAIR,    RESET,          
+		_______, _______, _______, _______, _______, _______, _______,                                 KC_AUDIO_VOL_UP,_______, _______, _______, _______, _______, _______, 
+		_______, _______, _______, _______, _______, _______, _______,                                                                 KC_AUDIO_VOL_DOWN,_______, _______, _______, RGB_MOD,        RGB_TOG,        _______, 
+		_______, _______, _______, _______, _______, _______,                                 _______, _______, RGB_HUD,        RGB_VAD,        RGB_VAI,        RGB_HUI,        
+		LGUI(LSFT(KC_4)),_______, LGUI(LCTL(LSFT(KC_4))),_______, LGUI(LSFT(KC_5)),LGUI(LCTL(KC_Q)),                                                                                                _______, KC_MEDIA_PLAY_PAUSE,KC_MEDIA_PREV_TRACK,KC_BRIGHTNESS_DOWN,KC_BRIGHTNESS_UP,KC_MEDIA_NEXT_TRACK,
+		_______, _______, _______,                 _______, _______, _______
+	),
+	[3] = LAYOUT_moonlander(
+		_______, _______, _______, _______, _______, _______, _______,                                 _______, _______, _______, _______, _______, _______, _______, 
+		_______, _______, _______, LALT(LGUI(LCTL(LSFT(KC_UP)))),_______, _______, _______,                                 _______, _______, _______, _______, _______, _______, _______, 
+		_______, _______, LALT(LGUI(LCTL(LSFT(KC_LEFT)))),LALT(LCTL(KC_C)),LALT(LGUI(LCTL(LSFT(KC_RIGHT)))),LALT(LGUI(LCTL(KC_RIGHT))),_______,                                                                 _______, _______, _______, _______, _______, _______, _______, 
+		_______, _______, _______, LALT(LGUI(LCTL(LSFT(KC_DOWN)))),_______, _______,                                 _______, _______, _______, _______, _______, _______, 
+		_______, _______, _______, _______, _______, _______,                                                                                                 _______, _______, _______, _______, _______, _______, 
+		_______, _______, _______,                 _______, _______, _______
+	),
+	[4] = LAYOUT_moonlander(
+		_______, _______, _______, _______, _______, _______, _______,                                 _______, _______, _______, _______, _______, _______, _______, 
+		_______, KC_TILD,        KC_GRAVE,       KC_LCBR,        KC_RCBR,        KC_PERC,        _______,                                 _______, KC_CIRC,        _______, _______, _______, _______, _______, 
+		_______, KC_EQUAL,       KC_MINUS,       KC_LPRN,        KC_RPRN,        KC_PLUS,        _______,                                                                 _______, KC_DLR,         KC_EXLM,        _______, _______, KC_COLN,        _______, 
+		_______, _______, KC_UNDS,        KC_LBRACKET,    KC_RBRACKET,    _______,                                 _______, _______, _______, _______, _______, _______, 
+		_______, _______, _______, _______, _______, _______,                                                                                                 _______, _______, _______, _______, _______, _______, 
+		_______, _______, _______,                 _______, _______, _______
+	),
+	[5] = LAYOUT_moonlander(
+		_______, _______, _______, _______, _______, _______, _______,                                 _______, _______, _______, KC_EQUAL,       KC_KP_SLASH,    KC_KP_ASTERISK, _______, 
+		_______, _______, _______, _______, _______, _______, _______,                                 _______, _______, KC_KP_7,        KC_KP_8,        KC_KP_9,        KC_KP_MINUS,    _______, 
+		_______, _______, _______, _______, _______, _______, _______,                                                                 _______, _______, KC_KP_4,        KC_KP_5,        KC_KP_6,        KC_KP_PLUS,     _______, 
+		_______, _______, _______, _______, _______, _______,                                 _______, KC_KP_1,        KC_KP_2,        KC_KP_3,        KC_KP_ENTER,    _______, 
+		_______, _______, _______, _______, _______, _______,                                                                                                 _______, KC_KP_0,        KC_BSPACE,      KC_KP_DOT,      _______, _______, 
+		_______, _______, _______,                 _______, _______, _______
+	),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
