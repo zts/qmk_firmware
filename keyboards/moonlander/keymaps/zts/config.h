02745ea0b9adef8c97d6aa220421f2318e66c477
Zachary Stevens <zts@akoova.com>
2023-06-11 16:57:21 +0100

    First pass add HRM to mini36
    Remove `IGNORE_MOD_TAP_INTERRUPT` - now the default behaviour.

--- keyboards/moonlander/keymaps/zts/config.h
+++ keyboards/moonlander/keymaps/zts/config.h
@@ -25,9 +25,6 @@
 #undef TAPPING_TERM
 #define TAPPING_TERM 216
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
-
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
 // END homerowmods
